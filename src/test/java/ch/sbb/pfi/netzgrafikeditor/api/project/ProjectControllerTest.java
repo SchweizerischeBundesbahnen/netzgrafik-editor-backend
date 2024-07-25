3d3e0dcdd76e737204668ec872b37f94954977a2
adrian_egli <adrian.egli@gmail.com>
2024-07-25 07:35:27 +0200

    fix

--- src/test/java/ch/sbb/pfi/netzgrafikeditor/api/project/ProjectControllerTest.java
+++ src/test/java/ch/sbb/pfi/netzgrafikeditor/api/project/ProjectControllerTest.java
@@ -1,8 +1,7 @@
 package ch.sbb.pfi.netzgrafikeditor.api.project;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.*;
 
 import java.util.regex.Matcher;
 
